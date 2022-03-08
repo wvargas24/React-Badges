22755d91afd3b609ed505ae07b7b357abbca7482
wvargas <wvargas@kambda.com>
2022-03-08 17:12:49 -0400

    Updates

---
+++ precache-manifest.a7eae8fa28c500866163bdfa437db021.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "f46836304286dac8b24b",
+    "url": "/React-Badges/static/css/main.e840655f.chunk.css"
+  },
+  {
+    "revision": "f46836304286dac8b24b",
+    "url": "/React-Badges/static/js/main.f4683630.chunk.js"
+  },
+  {
+    "revision": "5ef06b0e384c8026b0ca",
+    "url": "/React-Badges/static/js/runtime~main.5ef06b0e.js"
+  },
+  {
+    "revision": "56f48ef15f38f2b70439",
+    "url": "/React-Badges/static/css/2.e8fc3350.chunk.css"
+  },
+  {
+    "revision": "56f48ef15f38f2b70439",
+    "url": "/React-Badges/static/js/2.56f48ef1.chunk.js"
+  },
+  {
+    "revision": "f246f5f1fb702016606ee8c65e33c029",
+    "url": "/React-Badges/static/media/platziconf-logo.f246f5f1.svg"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/React-Badges/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/React-Badges/static/media/logo.0026b862.svg"
+  },
+  {
+    "revision": "71addf454967249472471f5398055e3d",
+    "url": "/React-Badges/static/media/astronauts.71addf45.svg"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/React-Badges/static/media/stars.1d17360a.svg"
+  },
+  {
+    "revision": "0b5785a5f111ea3a4ea07aab03c820b9",
+    "url": "/React-Badges/index.html"
+  }
+];
